cacd6329386108b691e6a1ea4ac7a81c4408b28a
diego <diegopgcds10@gmail.com>
2022-02-25 15:30:12 -0300

    learningPython - basic

---
+++ exerciciosBasicos.py
@@ -0,0 +1,105 @@
+
+# Atribuição e soma
+
+# num1 = 1
+# num2 = 2
+# num3 = 3
+# num4 = 4
+# num5 = 10
+#
+# print(num1+num2+num3+num4+num5)
+
+# Média
+
+# num1 = 23
+# num2 = 19
+# num3 = 31
+#
+# media = "a media do numero {}, do {} e do {} é: {}".format(num1, num2, num3, num1+num2+num3/3)
+#
+# print(media)
+
+# Media segunda forma
+
+# num1 = 23
+# num2 = 19
+# num3 = 31
+#
+# media = "a media do numero %.f, do %.f e do %.f é: %.f" % (num1, num2, num3, num1+num2+num3/3)
+#
+# print(media)
+
+# Resto da divisão
+
+# resto = 403 // 73
+#
+# print(resto)
+
+# Potencia maior
+
+# pot = 2**10
+#
+# print(pot)
+
+# Absoluto
+
+# print(abs(50 - 57))
+
+# Minímo
+
+# print(min(50, 58, 52, 32))
+
+# Atribuição e soma e multiplicação
+
+# a = 3
+# b = 4
+# c = a*a + b*b
+#
+# print(c)
+
+# Atribuição e concatenação
+
+# s1 = 'ant'
+# s2 = 'bat'
+# s3 = 'cod'
+#
+# print(s1 + ' ' + s2 + ' ' + s3)
+
+# s1 = 'ant'
+# s2 = 'bat'
+# s3 = 'cod'
+#
+# print(10*(s1 + s2 + s3 + ' '))
+
+# Percentual de desconto de valor
+
+# preco = float(input('entre com o valor do produto: '))
+# desconto = float(input('entre com o percentual do desconto: '))
+#
+# percentual = desconto / 100
+# precoFinal = preco - (percentual * preco)
+#
+# print('o valor do desconto é de {}%, e o preco final é de {}'.format(desconto, precoFinal))
+
+# Km rodados pelo carro e calculo a pagar com input, atribuição, etc
+
+# carro custa 60 por dia
+# 0,15 por km rodado
+
+# km = int(input('entre com a quantidade de km rodados pelo carro: '))
+# dias = int(input('entre com a quantidade de dias em que o carro foi usado: '))
+#
+# precoFinal = 60 * dias + 0.15 * km
+#
+# print('o preco final a pagar seria de: {}'.format(precoFinal))
+
+# pegar o tamanho de uma frase com a função len
+
+# frase = input('digite a frase:')
+# length = len(frase)
+#
+# frase2 = frase[:int(length/2)]
+#
+# print(frase2)
+
+
